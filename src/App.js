64e81b6333165c9384303894c1a2b05aff709a7a
AngelRSanchez <lenagzacnes@gmail.com>
2020-11-20 13:59:10 -0400

    Agregado el logo, animaciones y estilos globales

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Fragment } from 'react';
 import { ListOfCategories } from './components/ListOfCategories';
-import { GlobalStyle } from './GlobalStyles';
+import { GlobalStyle } from './styles/GlobalStyles';
 import { ListOfPhotoCards } from './components/ListOfPhotoCards';
+import { Logo } from './components/Logo';
 
 export const App = () => (
     <Fragment>
         <GlobalStyle />
+        <Logo />
         <ListOfCategories />
         <ListOfPhotoCards />
     </Fragment>
