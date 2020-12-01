8f7351d7397c8b04a86f44d9043bfd0fd2b7c7bf
AngelRSanchez <lenagzacnes@gmail.com>
2020-12-01 18:50:46 -0400

    Lazy load con intersection observer, configuracion de eslint mejorada, localstorage para guardar los likes localmente

--- src/components/Photocard/index.js
+++ src/components/Photocard/index.js
@@ -1,30 +1,54 @@
 import React, { Fragment, useEffect, useRef, useState } from "react";
 import { Article, ImgWrapper, Img, Button } from "./styles";
 
-import { MdFavoriteBorder } from "react-icons/md";
+import { MdFavoriteBorder, MdFavorite } from "react-icons/md";
 
 const DEFAULT_IMAGE =
   "https://res.cloudinary.com/midudev/image/upload/w_150/v1555671700/category_hamsters.jpg";
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   const element = useRef(null);
-
   const [show, setShow] = useState(false);
+  const key = `like-${id}`;
+  const [liked, setLiked] = useState(() => {
+    try {
+      const like = window.localStorage.getItem(key);
+      return like;
+    } catch (e) {
+      return false;
+    }
+  });
 
   useEffect(
     function () {
-      const observer = new window.IntersectionObserver(function (entries) {
-        const { isIntersecting } = entries[0];
-        console.log(isIntersecting)
-        if (isIntersecting) {
-          setShow(true);
-          observer.disconnect();
-        }
+      Promise.resolve(
+        typeof window.IntersectionObserver !== "undefined"
+          ? window.IntersectionObserver
+          : import("intersection-observer")
+      ).then(() => {
+        const observer = new window.IntersectionObserver(function (entries) {
+          const { isIntersecting } = entries[0];
+          if (isIntersecting) {
+            setShow(true);
+            observer.disconnect();
+          }
+        });
+        observer.observe(element.current);
       });
-      observer.observe(element.current);
     },
     [element]
   );
+
+  const Icon = liked ? MdFavorite : MdFavoriteBorder;
+
+  const setLocalStorage = value => {
+    try {
+      window.localStorage.setItem(key, value);
+      setLiked(value);
+    } catch (e) {
+      console.error(e);
+    }
+  }
 
   return (
     <Article ref={element}>
@@ -36,8 +60,8 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
             </ImgWrapper>
           </a>
 
-          <Button>
-            <MdFavoriteBorder size="24px" /> {likes} likes!
+          <Button onClick={() => setLocalStorage(!liked)}>
+            <Icon size="24px" /> {likes} likes!
           </Button>
         </Fragment>
       )}
